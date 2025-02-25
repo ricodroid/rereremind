757e014e0e2f3efb6b8ea190957c1504f757c594
ricodroid404 <ricodroid2024@gmail.com>
2025-02-24 19:28:30 -0500

    Initial Commit

---
+++ rereremind/rereremindApp.swift
@@ -0,0 +1,17 @@
+//
+//  rereremindApp.swift
+//  rereremind
+//
+//  Created by riko on 2025/02/24.
+//
+
+import SwiftUI
+
+@main
+struct rereremindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
